8b5744e6ddcd673e5ff22c11f0c99036d9e17ec8
manchukonda9 <krish9@GopiKrishnasMBP.cable.rcn.com>
2020-01-26 23:52:16 -0500

    Updates

---
+++ precache-manifest.96ed3edc8d9931e13b9cadd6881edd8f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f46bcc7f1a52d8e784bd312174e39ce9",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "801badf48b10063552ad",
+    "url": "/myportfolio/static/css/main.da005ed0.chunk.css"
+  },
+  {
+    "revision": "02011895e807b3e6f3f8",
+    "url": "/myportfolio/static/js/2.f839ce89.chunk.js"
+  },
+  {
+    "revision": "801badf48b10063552ad",
+    "url": "/myportfolio/static/js/main.e6179729.chunk.js"
+  },
+  {
+    "revision": "ab7a6d73eac9d7c1f116",
+    "url": "/myportfolio/static/js/runtime~main.630241ff.js"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/myportfolio/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "28ef278cebc2b599aabbbb0b49e887cc",
+    "url": "/myportfolio/static/media/d1.28ef278c.jpeg"
+  },
+  {
+    "revision": "8d3432faab8bf101f5938510352e9439",
+    "url": "/myportfolio/static/media/d2.8d3432fa.jpeg"
+  },
+  {
+    "revision": "f12b1aaaf504856e2733989d3c27a878",
+    "url": "/myportfolio/static/media/d3.f12b1aaa.jpeg"
+  },
+  {
+    "revision": "151f890e36b664c39d3ae1b479371fc5",
+    "url": "/myportfolio/static/media/d4.151f890e.jpeg"
+  },
+  {
+    "revision": "34263414a4ae5c3e0af758cd31356535",
+    "url": "/myportfolio/static/media/dp2.34263414.jpeg"
+  },
+  {
+    "revision": "4e3b3c4d363e049e755793f18c77da2d",
+    "url": "/myportfolio/static/media/fav1.4e3b3c4d.jpg"
+  },
+  {
+    "revision": "b21e7c4582f6ec44808014c9ea1d0143",
+    "url": "/myportfolio/static/media/fav2.b21e7c45.jpeg"
+  },
+  {
+    "revision": "d343abb2f4990e34a8fd057391175fa3",
+    "url": "/myportfolio/static/media/fav4.d343abb2.jpeg"
+  },
+  {
+    "revision": "74ac148db0bedda6359a11c2c16b3e2e",
+    "url": "/myportfolio/static/media/fav5.74ac148d.jpg"
+  },
+  {
+    "revision": "026b46bd7d0c9bbd1126d53baa05d672",
+    "url": "/myportfolio/static/media/fav6.026b46bd.jpg"
+  },
+  {
+    "revision": "baf6b40a654b078399e93e3d9cb6d455",
+    "url": "/myportfolio/static/media/profile-bg.baf6b40a.jpg"
+  },
+  {
+    "revision": "cf8b686b294041d0925f4e745b1fabb9",
+    "url": "/myportfolio/static/media/sign.cf8b686b.jpg"
+  }
+]);
